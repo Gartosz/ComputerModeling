123ca662b48a38cb262cdcc5bfb4c51836199a9a
Bartosz <bartekg001@gmail.com>
2023-03-07 13:29:49 +0100

    Copy source code from previous task.

---
+++ Lista_1/Game_of_life_statistics_ex2/src/ofApp.cpp
@@ -0,0 +1,111 @@
+#include "ofApp.h"
+
+#define KEY_SPACE 32
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+}
+
+void ofApp::update_cells()
+{
+    for (int col = 0; col < saved_generation.size(); ++col)
+    {
+        for (int row = 0; row < saved_generation[col].size(); ++row)
+        {
+            int neighbours = 0;
+            for (auto &pos : pos_to_check)
+                if (col + pos.first > 0 && col + pos.first < saved_generation.size() && row + pos.second > 0 && row + pos.second < saved_generation[col].size())
+                    if (saved_generation[col + pos.first][row + pos.second])
+                        ++neighbours;
+            if (saved_generation[col][row])
+                cells_matrix[col][row] = !(neighbours < 2 || neighbours > 3);
+
+            else if (neighbours == 3)
+                cells_matrix[col][row] = true;
+
+        }
+    }
+    saved_generation = cells_matrix;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    if (!paused)
+        update_cells();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    for (int col = 0; col < cells_matrix.size(); ++col)
+    {
+        for (int row = 0; row < cells_matrix[col].size(); ++row)
+            if (cells_matrix[col][row])
+                ofDrawRectangle(row * size, col * size, size, size);
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if (key == KEY_SPACE)
+        paused = !paused;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+void ofApp::insert_cell(int y, int x)
+{
+    x /= size;
+    y /= size;
+    if (y > 0 && y < cells_matrix.size() && x > 0 && x < cells_matrix[0].size())
+        cells_matrix[y][x] = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+   insert_cell(y, x);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    insert_cell(y, x);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ Lista_1/Game_of_life_statistics_ex2/src/ofApp.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "ofMain.h"
+#include <vector>
+#include <glm/gtc/random.hpp>
+
+class ofApp : public ofBaseApp{
+
+	public:
+		ofApp(int _size = 6, float chance_to_live = 0.2f)
+		{
+			ofSetFrameRate(30);
+			size = _size;
+			int height = ofGetHeight() / size;
+			int width = ofGetWidth() / size;
+			for (int y = 0; y < height; ++y)
+			{
+				cells_matrix.push_back(std::vector<bool>{});
+				for (int x = 0; x < width; ++x)
+					cells_matrix[y].push_back(glm::linearRand(0, 1000) < chance_to_live * 1000 ? true : false);
+			}
+			saved_generation = cells_matrix;
+		}
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		void update_cells();
+		void insert_cell(int y, int x);
+		
+
+	private:
+		int size = 6;
+		std::vector<std::vector<bool>> cells_matrix{};
+		std::vector<std::vector<bool>> saved_generation{};
+		std::vector<std::pair<int, int>> pos_to_check = {{-1, -1}, {-1, -0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+		bool paused = true;
+};
