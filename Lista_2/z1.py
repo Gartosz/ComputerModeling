83f533f31c6c8388ea3810f30ddcd91a9f7d5156
Bartosz <bartekg001@gmail.com>
2023-03-15 00:59:16 +0100

    Sort dictionary by values.

--- Lista_2/z1.py
+++ Lista_2/z1.py
@@ -1,4 +1,5 @@
 from re import sub
+from itertools import groupby
 
 if __name__ == "__main__":
     words_dict = {}
@@ -6,4 +7,5 @@ if __name__ == "__main__":
         words = file.read().split()
         words = [sub(r'\W+', '', word.lower()) for word in words]
         words_dict = {word: words.count(word) for word in set(words)}
+    words_dict = dict(sorted(words_dict.items(), reverse=True, key=lambda item: item[1]))
     print(words_dict)
