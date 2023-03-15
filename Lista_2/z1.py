3f01cf8efb0c03861b1ca664b565c847de8942c5
Bartosz <bartekg001@gmail.com>
2023-03-15 15:39:06 +0100

    Create dict of ranks and words with count of their occurences.

--- Lista_2/z1.py
+++ Lista_2/z1.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
         words = [sub(r'\W+', '', word.lower()) for word in words]
         words_dict = {word: words.count(word) for word in set(words)}
     words_dict = dict(sorted(words_dict.items(), reverse=True, key=lambda item: item[1]))
-    print(words_dict)
+    words_ranks = {rank + 1: (word, words_dict[word]) for rank, word in enumerate(words_dict.keys())}
+    print(words_ranks)
