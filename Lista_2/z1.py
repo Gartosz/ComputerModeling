9c69279771e824d8c5ad54dfeeb7d170de1461bb
Bartosz <bartekg001@gmail.com>
2023-03-21 23:12:03 +0100

    Analyse texts in loop.

--- Lista_2/z1.py
+++ Lista_2/z1.py
@@ -2,14 +2,16 @@ from re import sub
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    words_dict = {}
-    with open("test_file.txt", "r") as file:
-        words = file.read().split()
-        words = [sub(r'\W+', '', word.lower()) for word in words]
-        words_dict = {word: words.count(word) for word in set(words)}
-    words_dict = dict(sorted(words_dict.items(), reverse=True, key=lambda item: item[1]))
-    words_ranks = {rank + 1: (word, words_dict[word]) for rank, word in enumerate(words_dict.keys())}
-    plt.plot(words_ranks.keys(), list(zip(*words_ranks.values()))[1])
-    plt.xlabel("Liczba wystąpień")
-    plt.ylabel("Ranga")
-    plt.show()
+    files = ["tekst_1.txt"]
+    for file_name in files:
+        words_dict = {}
+        with open(file_name, "r", encoding="utf-8") as file:
+            words = file.read().split()
+            words = [sub(r'\W+', '', word.lower()) for word in words]
+            words_dict = {word: words.count(word) for word in set(words)}
+        words_dict = dict(sorted(words_dict.items(), reverse=True, key=lambda item: item[1]))
+        words_ranks = {rank + 1: (word, words_dict[word]) for rank, word in enumerate(words_dict.keys())}
+        plt.plot(words_ranks.keys(), list(zip(*words_ranks.values()))[1])
+        plt.xlabel("Liczba wystąpień")
+        plt.ylabel("Ranga")
+        plt.show()
