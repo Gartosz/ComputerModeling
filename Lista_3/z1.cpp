49cd12df16370cf1293b9f66408066b31cfe4d4c
Bartosz <bartekg001@gmail.com>
2023-03-28 00:56:35 +0200

    Set required data.

--- Lista_3/z1.cpp
+++ Lista_3/z1.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cmath>
 class System_2d
 {
     public:
@@ -10,6 +11,17 @@ class System_2d
         std::fill(masses.begin(), masses.end(), mass);
     }
 
+    void set_position(std::size_t index, double x_value, double y_value)
+    {
+        positions[index].first = x_value;
+        positions[index].second = y_value;
+    }
+
+    void set_velocity(std::size_t index, double x_value, double y_value)
+    {
+        velocities[index].first = x_value;
+        velocities[index].second = y_value;
+    }
     std::size_t n = 0;
     double dt = 0.1;
     std::vector<std::pair<double, double>> positions{};
@@ -23,5 +35,9 @@ int main()
     System_2d system(2);
     std::size_t R = 5;
 
+    system.positions[1].first = R;
+    system.set_position(1, R, 0);
+    system.set_velocity(1, 0, sqrt(system.masses[0]/R));
+    system.rigid[0] = false;
     return 0;
 }
