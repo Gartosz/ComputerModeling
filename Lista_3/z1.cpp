0668274ce6ae7825385694ae0ee31171363f8fd3
Bartosz <bartekg001@gmail.com>
2023-03-28 00:59:27 +0200

    Implement basic force updating.

--- Lista_3/z1.cpp
+++ Lista_3/z1.cpp
@@ -34,12 +34,50 @@ class System_2d
         velocities[index].first = x_value;
         velocities[index].second = y_value;
     }
+    void begin(std::size_t duration)
+    {
+        for (double time = 0; time <= duration; time += dt)
+            for (std::size_t i = 0; i < positions.size(); ++i)
+                if (rigid[i])  
+                {
+                    std::vector<double> force {};
+                    for (std::size_t j = 0; j < positions.size(); ++j)
+                        if (j != i)
+                            update_force(force, calculate_force(std::make_pair(i, j)));
+                }
+    }
+
     std::size_t n = 0;
     double dt = 0.1;
     std::vector<std::pair<double, double>> positions{};
     std::vector<std::pair<double, double>> velocities{};
     std::vector<double> masses {};
     std::vector<bool> rigid{true};
+
+    private:
+
+    void update_force(std::vector<double> &force, std::vector<double> comp_force)
+    {
+        force.insert(std::end(force), std::begin(comp_force), std::end(comp_force));
+    }
+    std::vector<double> calculate_force(std::pair<std::size_t, std::size_t> indexes)
+    {
+        double temp_force = (1*masses[indexes.first]*masses[indexes.second])/pow(distance(positions[indexes.first], positions[indexes.second]), 3);
+        return distance_vector(positions[indexes.first], positions[indexes.second]) * temp_force;
+    }
+
+    double distance(std::pair<double, double> pos1, std::pair<double, double> pos2)
+    {
+        return sqrt(pow(pos2.first - pos1.first, 2)+pow(pos2.second - pos1.second, 2));
+    }
+
+    std::vector<double> distance_vector(std::pair<double, double> pos1, std::pair<double, double> pos2)
+    {
+        std::vector<double> result(2);
+        result[0] = pos2.first - pos1.first;
+        result[1] = pos2.second - pos1.second;
+        return result;
+    }
 };
 
 int main()
