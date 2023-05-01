af2732594e597d3864a3a8f71bce1b08e16e22ca
Bartosz <bartekg001@gmail.com>
2023-05-01 14:11:19 +0200

    Simplify generation of 1D walk and get only the last position.

---
+++ Lista_4/Zadanie3/z3.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <ctime>
+#include <stdio.h>
+#include <string>
+#include <random>
+#include <vector>
+int generate_1D_walk(std::mt19937 &rng, std::uniform_int_distribution<int> &distribution, std::size_t iterations = 1000)
+{
+    int pos_x = 0;
+    for (int i = 0; i < iterations; ++i)
+        pos_x += distribution(rng) ? 1 : -1;
+
+    return pos_x;
+}
+
+int main()
+{
+
+    return 0;
+}
