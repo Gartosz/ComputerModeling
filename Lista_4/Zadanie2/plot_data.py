b189a5c169a5aaa436a6d04772a3eab17987b414
Bartosz <bartekg001@gmail.com>
2023-05-01 14:09:35 +0200

    Plot random walks to file.

--- Lista_4/Zadanie2/plot_data.py
+++ Lista_4/Zadanie2/plot_data.py
@@ -4,3 +4,14 @@ from numpy import loadtxt
 
 if __name__ == "__main__":
     dirs = [glob("1D/*")[-1], glob("2D/*")[-1]]
+    for walk in dirs:
+        plt.xlabel("time" if "1D" in walk else "x")
+        plt.ylabel("y")
+        plt.title(walk.split("\\")[0] + " random walk")
+        for file_name in glob(walk + "/*.txt"):
+            print(file_name)
+            x, y = loadtxt(file_name, delimiter=' ', unpack=True)
+            plt.plot(x, y)
+        plt.legend(range(0, 10))
+        plt.savefig(walk + "\\" + walk.split("\\")[1] + ".svg", format = "svg")
+        plt.close()
