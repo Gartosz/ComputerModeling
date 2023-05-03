27b483e74dafe9d93b3103b058d4ce50fcc3a93f
Bartosz <bartekg001@gmail.com>
2023-05-03 20:30:16 +0200

    Modify 2D walk generator function to return array pointer.

--- Lista_4/Zadanie4/z4.cpp
+++ Lista_4/Zadanie4/z4.cpp
@@ -13,6 +13,21 @@ int generate_1D_walk(std::mt19937 &rng, std::uniform_int_distribution<int> &dist
     return pos_x;
 }
 
+int* generate_2D_walk(std::mt19937 &rng, std::uniform_int_distribution<int> &distribution, std::size_t iterations = 10000)
+{
+    std::vector<std::vector<int>> moves{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int pos[2] = {0, 0};
+    pos[0] = 0;
+    pos[1] = 0;
+    std::vector<int> next_move{0, 0};
+    for (int i = 0; i < iterations; ++i)
+    {
+        next_move = moves[distribution(rng)];
+        pos[0] += next_move[0];
+        pos[1] += next_move[1];
+    }
+    return pos;
+}
 
 int main()
 {
