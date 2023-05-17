984093d77b16c3ec0f91a858fdbb7c59bc46f014
Bartosz <bartekg001@gmail.com>
2023-05-17 10:46:11 +0200

    Create class to handle position.

--- Lista_5/z1.cpp
+++ Lista_5/z1.cpp
@@ -1,5 +1,14 @@
 #include <fstream>
 #include <vector>
+class Position
+{
+    public:
+    Position(int _x, int _y) : x(_x), y(_y){}
+
+    private:
+    int x = 0;
+    int y = 0;
+};
 
 class EdensGrowthModel
 {
