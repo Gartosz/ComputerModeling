7d46a597fe02763c04b86979713cae61a613b411
Bartosz <bartekg001@gmail.com>
2023-05-28 13:41:49 +0200

    Change type of vector.

--- Lista_5/z1.cpp
+++ Lista_5/z1.cpp
@@ -62,12 +62,12 @@ class EdensGrowthModel
     public:
     EdensGrowthModel()
     {
-        cells.push_back(Position(0,0));
+        cells.push_back(Cell(0, 0, 0));
         free_spaces_cells.push_back(0);
     }
 
     private: 
-    std::vector<Position> cells{};
+    std::vector<Cell> cells{};
     std::vector<size_t> free_spaces_cells{};
     std::random_device dev;
     uint32_t seed = dev();
